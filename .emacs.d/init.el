c6b31432d646291b7b253d4c4e3bdc7bf7b8bc5d
Brian Lemberger <brian.lemberger@gmail.com>
2020-05-10 16:05:26 -0500

    Removed cruft

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -86,8 +86,7 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (which-key use-package)))
- '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e"))))
+ '(package-selected-packages (quote (which-key use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
