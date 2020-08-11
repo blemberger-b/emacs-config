00418384d25282ba991eb70d5283802e99bb8e3f
Brian Lemberger <brian.lemberger@gmail.com>
2020-08-10 22:43:59 -0500

    Trying to get rid of Monaco font setting

--- init.el
+++ init.el
@@ -49,7 +49,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :height 140 :foundry "nil" :family "Monaco"))))
  '(minibuffer-prompt ((t (:foreground "white" :weight bold))))
  (unless window-system
    '(show-paren-match ((t (:background "blue" :foreground "white")))))
