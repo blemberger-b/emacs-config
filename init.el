48e933f2715f7762eb0d562cb9323c0b3b7a5a3c
Brian Lemberger <brian.lemberger@climate.com>
2022-07-06 11:28:43 -0500

    Added protobuf-mode & changed auto-dim customizations

--- init.el
+++ init.el
@@ -61,7 +61,7 @@
      ("XXX+" . "#dc752f")
      ("\\?\\?\\?+" . "#dc752f")))
  '(package-selected-packages
-   '(auto-dim-other-buffers org-bullets pcre2el plantuml-mode general smex with-editor elisp-refs page-break-lines org-tree-slide projectile yaml-mode beacon markdown-mode transient async clojure-mode dash golden-ratio-scroll-screen magit gnu-elpa-keyring-update htmlize mwim goto-last-change popup-kill-ring diminish spaceline company rainbow-delimiters presentation sudo-edit paredit cider which-key use-package))
+   '(protobuf-mode auto-dim-other-buffers org-bullets pcre2el plantuml-mode general smex with-editor elisp-refs page-break-lines org-tree-slide projectile yaml-mode beacon markdown-mode transient async clojure-mode dash golden-ratio-scroll-screen magit gnu-elpa-keyring-update htmlize mwim goto-last-change popup-kill-ring diminish spaceline company rainbow-delimiters presentation sudo-edit paredit cider which-key use-package))
  '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e")))
 
 (custom-set-faces
