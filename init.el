53e9129448b3f66653501739e540d7f8337cfba4
Brian Lemberger <brian.lemberger@climate.com>
2023-07-05 08:48:31 -0500

    Enabled god-mode

--- init.el
+++ init.el
@@ -72,6 +72,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((((type ns)) (:inherit nil :height 160 :foundry "nil" :family "Fira Code"))))
+ '(god-mode-lighter ((t (:inherit error))))
  '(dashboard-items-face ((t (:inherit widget-button :underline nil))))
  '(ediff-current-diff-A ((t (:extend t :background "#003f8e" :foreground "#7285b7"))))
  '(ediff-current-diff-B ((t (:extend t :background "#003f8e" :foreground "#7285b7"))))
